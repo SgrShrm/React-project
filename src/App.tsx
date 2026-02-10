ad3f98a0ad9396d6e338cabfb2a6b709c7593407
SgrShrm <sagarsharmass143143@gmail.com>
2026-02-10 08:43:28 +0545

    navabar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Router from "./routes/Index"
 
+
 function App() {
 
   return (
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import { SearchIcon, ShoppingCart, User } from "lucide-react";
+
 
 
 function Header () {
     return (
-        <div>Header</div>
+        <>
+        <div className="primary-color h-[44px] w-auto flex text justify-around items-center text-[16px]">
+       <div className="flex gap-x-3">
+          <div>test@test.com</div>
+          <div>9817211100</div>
+       </div>
+       <div>
+
+         <div className="flex">
+            
+               <select name="language" id="language" >
+                  <option value="nepali">Nepali</option>
+                  <option value="english">English</option>
+               </select>
+
+               <select name="currency" id="currency"  >
+                  <option value="NPR">NPR</option>
+                  <option value="USD">USD</option>
+               </select>
+               
+               <div className='gap-2 flex'>
+               <div >Login</div><User/>
+               <div>Cart</div><ShoppingCart/>
+               </div>
+           
+         </div>
+       </div>
+    </div>
+    <div className='h-[40px] w-[1177px] flex justify-between align-items-center ml-[371px] items-center my-4'>
+       <div className='font-style text-[34px]'>Hekto</div>
+       <div className='flex align-items-center'>
+        <ul className='flex gap-4'>
+            <li>Home</li>
+            <li>Pages</li>
+            <li>Products</li>
+        </ul>
+       </div>
+
+       <div className='flex h-[40px] w-[317px] border border-l-gray-400'>
+        <input type="search"className='border h-[40px] w-[317px]'/>
+        <div className='primary-color flex items-center w-[40px] justify-center'><SearchIcon/></div>
+        </div>
+
+    </div>
+    </> 
     )
 }
 
